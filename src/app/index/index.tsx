fedf6b63042bffcc6b2edc3628f42dde453707c7
Eduardo <eduardo_silva800@hotmail.com>
2025-10-25 15:50:40 -0300

    feat: add modal component for link details and update styles

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -2,7 +2,14 @@ import { Categories } from "@/components/categories";
 import { Link } from "@/components/link";
 import { colors } from "@/styles/colors";
 import { MaterialIcons } from "@expo/vector-icons";
-import { FlatList, Image, TouchableOpacity, View } from "react-native";
+import {
+  FlatList,
+  Image,
+  Modal,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import styles from "./styles";
 
 export default function Index() {
@@ -30,6 +37,26 @@ export default function Index() {
         contentContainerStyle={styles.linksContent}
         showsVerticalScrollIndicator={false}
       />
+
+      <Modal transparent visible={true}>
+        <View style={styles.modal}>
+          <View style={styles.modalContent}>
+            <View style={styles.modalHeader}>
+              <Text style={styles.modalCategory}>Curso</Text>
+              <TouchableOpacity>
+                <MaterialIcons
+                  name="close"
+                  size={20}
+                  color={colors.gray[400]}
+                />
+              </TouchableOpacity>
+            </View>
+
+            <Text style={styles.modalLinkName}>MyPanicFull</Text>
+            <Text style={styles.modalUrl}>https://mypanicfull.com</Text>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
