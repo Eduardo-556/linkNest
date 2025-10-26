9b7c1370b6fd88d4a5c38020beae0346f2d0d89e
Eduardo <eduardo_silva800@hotmail.com>
2025-10-26 12:59:25 -0300

    feat: add validation to the Add component form inputs

--- src/app/add/index.tsx
+++ src/app/add/index.tsx
@@ -6,7 +6,7 @@ import { colors } from "@/styles/colors";
 import { MaterialIcons } from "@expo/vector-icons";
 import { router } from "expo-router";
 import { useState } from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Alert, Text, TouchableOpacity, View } from "react-native";
 
 export default function Add() {
   const [category, setCategory] = useState("");
@@ -14,7 +14,16 @@ export default function Add() {
   const [url, setUrl] = useState("");
 
   function handlerAdd() {
-    console.log("Adicionando link:", { name, url });
+    if (!name.trim()) {
+      return Alert.alert("Nome", "Informe um nome para a URL");
+    }
+    if (!url.trim()) {
+      return Alert.alert("URL", "Informe uma URL");
+    }
+    if (!category) {
+      return Alert.alert("Categoria", "Selecione uma categoria para a URL");
+    }
+    console.log("Adicionando link:", { category, name, url });
   }
 
   return (
