7986897719a7d20b7b5fce03895c3cc164c4b4fd
Eduardo <eduardo_silva800@hotmail.com>
2025-10-28 22:04:37 -0300

    feat: implement link removal functionality and update modal behavior

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -39,6 +39,23 @@ export default function Index() {
     setLink(selected);
   }
 
+  async function linkRemove() {
+    try {
+      await linkStorage.remove(link.id);
+      getLinks();
+      setShowModal(false);
+    } catch (error) {
+      Alert.alert("Erro", "Não foi possível excluir.");
+    }
+  }
+
+  function handleRemove() {
+    Alert.alert("Excluir", "Deseja realmente excluir este link?", [
+      { style: "cancel", text: "Não" },
+      { style: "destructive", text: "Sim", onPress: linkRemove },
+    ]);
+  }
+
   useFocusEffect(
     useCallback(() => {
       getLinks();
@@ -92,7 +109,12 @@ export default function Index() {
             <Text style={styles.modalLinkName}>{link.name}</Text>
             <Text style={styles.modalUrl}>{link.url}</Text>
             <View style={styles.modalFooter}>
-              <Option name="Excluir" icon="delete" variant="secondary" />
+              <Option
+                name="Excluir"
+                icon="delete"
+                variant="secondary"
+                onPress={handleRemove}
+              />
               <Option name="Abrir" icon="language" />
             </View>
           </View>
--- src/storage/link-storage.ts
+++ src/storage/link-storage.ts
@@ -27,7 +27,18 @@ async function save(newLink: LinkStorage) {
   }
 }
 
+async function remove(id: string) {
+  try {
+    const storage = await get();
+    const upadated = storage.filter((link) => link.id != id);
+    await AsyncStorage.setItem(LINKS_STORAGE_KEY, JSON.stringify(upadated));
+  } catch (error) {
+    throw error;
+  }
+}
+
 export const linkStorage = {
   get,
   save,
+  remove,
 };
